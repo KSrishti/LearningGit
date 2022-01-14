fc91caa54686aa9a117f77232596422015f7a84b
Srishti <srishtidy@gmail.com>
2022-01-14 11:10:26 +0530

    ne more commit

--- second.java
+++ second.java
@@ -2,6 +2,6 @@ class TryingGit2
 {
     public static void main(String args[])
     {
-        System.out.println("Tryinngg my Second Git ");
+        System.out.println("Tryingggg my Second Git ");
     }
 }
