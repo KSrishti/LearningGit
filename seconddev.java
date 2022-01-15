5e3000fa9e74d67db3bd01b5049aeebe8e35f8fd
Srishti <srishtidy@gmail.com>
2022-01-15 09:41:39 +0530

    NOO

--- seconddev.java
+++ seconddev.java
@@ -2,6 +2,6 @@ class TryingGit2
 {
     public static void main(String args[])
     {
-        System.out.println(" my Seconddev Git ");
+        System.out.println(" my Seconddev Git: ");
     }
 }
